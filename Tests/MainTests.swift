e27bad82b8491691390f7b0e65f0898e6773c79c
Nikita Ermolenko <gnod94@gmail.com>
2017-06-11 14:10:06 +0600

    Change bundle id, product name and deployment versions

---
+++ Tests/MainTests.swift
@@ -0,0 +1,21 @@
+//
+//  MainTests.swift
+//  CodableAlamofire
+//
+//  Created by Nikita Ermolenko on 11/06/2017.
+//
+
+import XCTest
+
+class MainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
