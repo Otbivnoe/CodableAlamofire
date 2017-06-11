53ec5e5a4fec4eabd3312021be7d934f1b39199a
Nikita Ermolenko <gnod94@gmail.com>
2017-06-11 14:04:14 +0600

    Create framework for all platforms

---
+++ Sources/Supporting Files/CodableAlamofire.h
@@ -0,0 +1,18 @@
+//
+//  CodableAlamofire.h
+//  CodableAlamofire
+//
+//  Created by Nikita Ermolenko on 11/06/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableAlamofire.
+FOUNDATION_EXPORT double CodableAlamofireVersionNumber;
+
+//! Project version string for CodableAlamofire.
+FOUNDATION_EXPORT const unsigned char CodableAlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableAlamofire/PublicHeader.h>
+
+
