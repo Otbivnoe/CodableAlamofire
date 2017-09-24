e6c405c7777271da96e5653040dd6c1919b8a881
Nikita Ermolenko <gnod94@gmail.com>
2017-09-24 18:17:58 +0600

    Refactor project structure for SPM compatible

--- Package.swift
+++ Package.swift
@@ -4,7 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "CodableAlamofire",
-    dependencies : [
+    products: [
+        .library(name: "CodableAlamofire", targets: ["CodableAlamofire"]),
+    ],
+    dependencies: [
    		.package(url: "https://github.com/Alamofire/Alamofire", from: "4.5.0")
-    ]
+    ],	
+    targets: [
+    	.target(
+			name: "CodableAlamofire", 
+    		dependencies: ["Alamofire"]
+    	),
+    	.testTarget(
+	    	name: "CodableAlamofireTests", 
+	    	dependencies: ["CodableAlamofire"]
+	    )
+  	]
 )
